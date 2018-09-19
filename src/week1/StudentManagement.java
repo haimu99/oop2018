7dc280eca7db286c8db46ad438145d482b8fb8f9
Nguyễn Minh Hải <haimu.99@gmail.com>
2018-09-19 23:29:58 +0700

    week 1

--- src/week1/StudentManagement.java
+++ src/week1/StudentManagement.java
@@ -5,10 +5,9 @@ public class StudentManagement {
 
 
 	// TODO: khai báo thuộc tính students là array chứa các đối tượng thuộc lớp Student (max. 100)
-
-    public boolean sameGroup(Student s1, Student s2) {
-        // TODO:
-        return false; // xóa dòng này sau khi cài đặt
+	public Student[] s_Array= new Student[100];
+    public static boolean sameGroup(Student s1, Student s2) {
+        return s1.getGroup().equals(s2.getGroup());
     }
 
     void studentsByGroup() {
@@ -27,9 +26,11 @@ public class StudentManagement {
     	s1.setGroup("INT22042");
     	s1.setEmail("haimu.99@gmail.com");
     	s1.getInfo();
-    	Student s2=new Student();
+    	Student s2=new Student(s1);
     	s2.getInfo();
-    	Student s3=new Student(s1);
+    	Student s3=new Student();
     	s3.getInfo();
+    	System.out.println("Sinh vien 1 v� 2 cung lop? "+sameGroup (s1,s2));
+    	System.out.println("Sinh vien 1 v� 3 cung lop? "+sameGroup (s1,s3));
     }
 }
