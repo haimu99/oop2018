4cde633de0c9f5db07c32d999c6d5dd7eef7e244
Nguyễn Minh Hải <haimu.99@gmail.com>
2018-10-05 20:23:55 +0700

    update

--- test/week3/Week3Test.java
+++ test/week3/Week3Test.java
@@ -42,38 +42,28 @@ public class Week3Test {
      */
     @Test
     public void testMax1() {
-        System.out.println("test max1");
+        System.out.println("test max 1");
         assertEquals(-3, Week3.max(-5, -3));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     @Test
-    public void testMax2() {
-        System.out.println("test max2");
+    public void testMax2(){
+        System.out.println("test max 2");
         assertEquals(0, Week3.max(0,-5));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     @Test
     public void testMax3(){
-        System.out.println("test max3");
+        System.out.println("test max 3");
         assertEquals(7, Week3.max(-5, 7));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     @Test
-    public void testMax4() {
-        System.out.println("test max4");
-        assertEquals(4, Week3.max(0, 4));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+    public void testMax4(){      
+        System.out.println("test max 4");
+        assertEquals(4, Week3.max(4, 4));
     }
     @Test
-    public void testMax5() {
-        System.out.println("test max5");
+    public void testMax5(){
+        System.out.println("test max 5");
         assertEquals(9, Week3.max(9, 3));
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
     }
     /**
      * Test of minOfArray method, of class Week3.
@@ -85,11 +75,7 @@ public class Week3Test {
         array[0]=-15;
         array[1]=-20;
         array[2]=6;
-        int expResult = -20;
-        int result = Week3.minOfArray(array);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype."); 
+        assertEquals(-20, Week3.minOfArray(array));
     }
     @Test
     public void testMinOfArray2() {
@@ -98,11 +84,7 @@ public class Week3Test {
         array[0]=-3;
         array[1]=9;
         array[2]=-10;
-        int expResult = -10;
-        int result = Week3.minOfArray(array);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        assertEquals(-10, Week3.minOfArray(array));
     }
     @Test
     public void testMinOfArray3() {
@@ -111,24 +93,16 @@ public class Week3Test {
         array[0]=-3;
         array[1]=0;
         array[2]=5;
-        int expResult = -3;
-        int result = Week3.minOfArray(array);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        assertEquals(-3, Week3.minOfArray(array));
     }
 @Test
     public void testMinOfArray4() {
         System.out.println("test minOfArray4");
         int[] array = new int[3];
-        array[0]=6;
+        array[0]=0;
         array[1]=0;
-        array[2]=15;
-        int expResult = 0;
-        int result = Week3.minOfArray(array);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        array[2]=0;
+        assertEquals(0, Week3.minOfArray(array));
     }   
     @Test
     public void testMinOfArray5() {
@@ -137,69 +111,34 @@ public class Week3Test {
         array[0]=5;
         array[1]=9;
         array[2]=9;
-        int expResult = 5;
-        int result = Week3.minOfArray(array);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+        assertEquals(5, Week3.minOfArray(array));
     }
     /**
      * Test of calculateBMI method, of class Week3.
      */
     @Test
     public void testCalculateBMI1() {
-        System.out.println("calculateBMI1");
-        double weight = 80;
-        double height = 1.78;
-        String expResult = "Béo phì";
-        String result = Week3.calculateBMI(weight, height);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }    
+        System.out.println("test BMI 1");
+        assertEquals("Béo phì", Week3.calculateBMI(80, 1.78));
+    }
     @Test
-    public void testCalculateBMI2() {
-        System.out.println("calculateBMI2");
-        double weight = 70;
-        double height = 1.7;
-        String expResult = "Thừa cân";
-        String result = Week3.calculateBMI(weight, height);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }    
+    public void testCalculateBMI2(){
+        System.out.println("test BMI 2");
+        assertEquals("Bình thường",Week3.calculateBMI(55, 1.7));
+    }
     @Test
-    public void testCalculateBMI3() {
-        System.out.println("calculateBMI3");
-        double weight = 55;
-        double height = 1.7;
-        String expResult = "Bình thường";
-        String result = Week3.calculateBMI(weight, height);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }    
+    public void testCalculateBMI3(){
+        System.out.println("test BMI 3");
+        assertEquals("Thừa cân",Week3.calculateBMI(70, 1.7));
+    }
     @Test
-    public void testCalculateBMI4() {
-        System.out.println("calculateBMI4");
-        double weight = 52;
-        double height = 1.7;
-        String expResult = "Thiếu cân";
-        String result = Week3.calculateBMI(weight, height);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
+    public void testCalculateBMI4(){
+        System.out.println("test BMI 4");
+        assertEquals("Thiếu cân", Week3.calculateBMI(52, 1.7));
+    }
+    @Test 
+    public void testCalculateBMI5(){
+        System.out.println("test BMI 5");
+        assertEquals("Thừa cân",Week3.calculateBMI(71, 1.71));
     }    
-    @Test
-    public void testCalculateBMI5() {
-        System.out.println("calculateBMI5");
-        double weight = 70;
-        double height = 1.7;
-        String expResult = "Thừa cân";
-        String result = Week3.calculateBMI(weight, height);
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        //fail("The test case is a prototype.");
-    }    
-    
 }
