c5104f3e6f610d39ff29f5ae0c095aeba001fef5
haimu99 <43139675+haimu99@users.noreply.github.com>
2018-11-23 08:46:09 +0700

    up

--- src/week11/Task1.java
+++ src/week11/Task1.java
@@ -1,4 +1,33 @@
 package week11;
 
 public class Task1 {
+	public static <T extends Comparable> void sort(T[] inputArray) {
+		// sap xep
+		for (int i = 0; i < inputArray.length; i++) {
+			for (int j = i + 1; j < inputArray.length; j++)
+				if (inputArray[i].compareTo(inputArray[j]) > 0) {
+					T x = inputArray[i];
+					inputArray[i] = inputArray[j];
+					inputArray[j] = x;
+				}
+		}
+
+	}
+
+	// in mảng
+	public static <T> void printArray(T[] inputArray) {
+		for (int i = 0; i < inputArray.length; i++) {
+			System.out.println(inputArray[i]);
+		}
+	}
+
+	public static void main(String args[]) {
+		Integer[] intArray = { 5, 1, 8, 9, 3 };
+		sort(intArray);
+		printArray(intArray);
+		Double[] doubleArray = { 1.5, 6.0, -0.5, 2.0, 7.0 };
+		sort(doubleArray);
+		printArray(doubleArray);
+
+	}
 }
--- src/week11/Task2.java
+++ src/week11/Task2.java
@@ -1,4 +1,19 @@
 package week11;
 
 public class Task2 {
+	public static <T extends Comparable> T findMax(T[] inputArray){
+		T max=inputArray[0];
+		for (int i=0;i<inputArray.length;i++) {
+			if(inputArray[i].compareTo(max)>0) {
+				max=inputArray[i];
+			}
+		}
+		return max;
+	}
+	public static void main(String args[]) {
+		Integer[] intArray= {3,6,8,1,-9};
+		System.out.println(findMax(intArray));
+		Double[] doubleArray = { 1.5, 6.0, -0.5, 2.0, 7.0 };
+		System.out.println(findMax(doubleArray));
+	}
 }
